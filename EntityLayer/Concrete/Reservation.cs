07bd7e197f09f8044d3a4caedef69cf6dde909cd
ertugrulchn <cihanertugrulemre368@gmail.com>
2022-06-21 04:43:06 +0300

    Reservations Entity Added

--- EntityLayer/Concrete/Reservation.cs
+++ EntityLayer/Concrete/Reservation.cs
@@ -8,5 +8,13 @@ namespace EntityLayer.Concrete
 {
     public class Reservation
     {
+        public int ReservationID { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public string PersonCount { get; set; }
+        public string Destination { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
     }
 }
